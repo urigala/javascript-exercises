b6b78654ca0ca0a361438b85da8e28eaca2aeb65
uriel g <urigalarza@hotmail.com>
2022-03-05 11:39:21 -0500

    Complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args ) {
+    let currentArg = 0
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < args.length; j++) {
+            if (array[i] === args[j]) {
+                array.splice(i, 1)
+                i-- // must reduce current iteration since element was removed
+                currentArg++
+            }
+        }        
+    }
+    return array
 };
+
+// removeFromArray([1, 2, 3, 4, 5, 6], 2, 3, 4, '6')
 
 // Do not edit below this line
 module.exports = removeFromArray;
