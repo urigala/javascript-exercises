6e601560f016fd01820ad1cdc42be9f13d975076
uriel g <urigalarza@hotmail.com>
2022-03-05 12:16:17 -0500

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(argOne, argTwo) {
 
+    if (argOne < 0 || argTwo < 0) {
+        return 'ERROR'
+    } else if (typeof argOne !== 'number' || typeof argTwo !== 'number' ) {
+        return 'ERROR'
+    }
+
+    let minNum
+    let maxNum
+    if (argOne < argTwo) {
+        minNum = argOne
+        maxNum = argTwo
+    } else {
+        minNum = argTwo
+        maxNum = argOne
+    }
+
+    let numSum = 0
+    for (let i = minNum; i <= maxNum; i++ ) {
+        numSum+=i
+    }
+
+    return numSum
 };
 
 // Do not edit below this line
