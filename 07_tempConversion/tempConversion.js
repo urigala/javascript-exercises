d508d0f05f3890ccd74fba39d20a9c5ae561aa6f
uriel g <urigalarza@hotmail.com>
2022-03-05 12:41:54 -0500

    Complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celciusTemp = (temp-32)*(5/9)  
+  return roundToOne(celciusTemp)
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let fahrenTemp = temp * (9/5) + 32
+  return roundToOne(fahrenTemp)
 };
+
+function roundToOne(num) {    
+  return +(Math.round(num + "e+1")  + "e-1");
+}
 
 // Do not edit below this line
 module.exports = {
