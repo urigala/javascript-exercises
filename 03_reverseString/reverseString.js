b2b3ea01a367f979c8050cc79824a1f4102b410b
uriel g <urigalarza@hotmail.com>
2022-03-05 10:54:50 -0500

    Complete 03_reverseString Exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+
+    let stringArr = string.split('');
+    stringArr.reverse();
+    let stringReversed = stringArr.join('');
+    return stringReversed;
 
 };
+
+reverseString('string')
 
 // Do not edit below this line
 module.exports = reverseString;
