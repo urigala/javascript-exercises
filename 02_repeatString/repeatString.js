3d4a68ce924968cc3e7d8a376686f6da140a262c
uriel g <urigalarza@hotmail.com>
2022-03-05 10:23:31 -0500

    Complete 02_repeatString Exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, number) {
+    if (number < 0) {
+        return 'ERROR'
+    }
+    let newString = ''
 
+    for (let i = 0; i < number; i++) {
+        newString+=string
+    }
+    return newString
 };
 
 // Do not edit below this line
