8a450066a86045a804459a1faed28cc04fc2a2c5
uriel g <urigalarza@hotmail.com>
2022-03-08 18:52:51 -0500

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let n = parseInt(x);
+    if (n < 0) {
+        return 'OOPS';
+    } else if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 };
 
 // Do not edit below this line
